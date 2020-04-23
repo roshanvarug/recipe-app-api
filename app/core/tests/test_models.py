1b2a32bec1f4159c00d469d698a168676355bff6
Your Name <you@example.com>
2020-04-23 11:22:06 +0530

    dgf

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,3 +24,13 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None,'test123')
+
+    def test_new_superuser(self):
+    """Test creating a new superuser"""
+    user = get_user_model().objects.create_superuser(
+        'test@londonappdev.com',
+        'test123'
+    )
+
+    self.assertTrue(user.is_superuser)
+    self.assertTrue(user.is_staff)
