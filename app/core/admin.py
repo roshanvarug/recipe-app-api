f0f69d9e004b8bc4748b84d974c497f7902ff5d1
Your Name <you@example.com>
2020-04-24 11:27:49 +0530

    we

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,6 +7,26 @@ from core import models
 class UserAdmi(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
-
+    fieldsets = (
+    (None, {'fields': ('email', 'password')}),
+    (_('Personal Info'), {'fields': ('name',)}),
+    (
+        _('Permissions'),
+        {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }
+    ),
+    (_('Important dates'), {'fields': ('last_login',)}),
+)
+    add_fieldsets = (
+    (None, {
+        'classes': ('wide',),
+        'fields': ('email', 'password1', 'password2')
+    }),
+)
 
 admin.site.register(models.User, UserAdmi)
